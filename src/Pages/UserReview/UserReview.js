afdd3276a6f81d76525e58b0997affdecd0804cb
Jahirul Islam <jahirul@elyranur.com>
2022-12-20 12:33:54 +0600

    clint side to server side data sent

--- src/Pages/UserReview/UserReview.js
+++ src/Pages/UserReview/UserReview.js
@@ -14,9 +14,42 @@ const UserReview = () => {
     }, [user?.email])
 
     return (
-        <div>
-            <h1 className='text-4xl'> You have {review.length} Review</h1>
-        </div>
+        <div className="overflow-x-auto w-full">
+  <table className="table w-full">
+    <tbody>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Hart Hagerty</div>
+              <div className="text-sm opacity-50">United States</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Zemlak, Daniel and Leannon
+          <br/>
+          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+        </td>
+        <td>Purple</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+    </tbody>
+    
+  </table>
+</div>
     );
 };
 
